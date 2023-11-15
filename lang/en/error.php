b6fbf055bc7013aaf5d86ca8f8d7064720a2e367
afshin-phpy <hassaniiafshin@gmail.com>
2023-11-15 23:00:43 +0330

    feat(utils): add DateFormatter utility and English localization file
    - DateFormatter added for retrunin date in a specific format which needed
    - error English file localization added for error messages

---
+++ lang/en/error.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    
+    'api'   =>  [
+        '502'   =>  'Unable to connect to the :APIName.',
+        '500'   =>  'An error occurred while fetching news.'
+    ],
+
+    'unexpected'    =>  'An unexpected error occurred.'
+];
