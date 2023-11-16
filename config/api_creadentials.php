3e105129ade1ddde955f98220075e86c2dc72902
afshin-phpy <hassaniiafshin@gmail.com>
2023-11-16 23:29:32 +0330

    refactor: Centralize environment configuration for API-key in the api_credentials config file

---
+++ config/api_creadentials.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'news_api_key'          =>  env("NEWSAPI_KEY"),
+    'guardian_api_key'      =>  env("GUARDIANAPI_KEY"),
+    'mediastack_api_key'    =>  env("MEDIASTACKAPI_KEY")
+];
