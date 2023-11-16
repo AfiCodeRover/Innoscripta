4b09baf2264e039743b4505a74bfa81a5aecc880
afshin-phpy <hassaniiafshin@gmail.com>
2023-11-16 19:57:27 +0330

    chore: Add api en localization file

---
+++ lang/en/api.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'required'              =>  'The :PARAM field is required.',
+    'string_type_error'     =>  'The :PARAM field must be a string.',
+    'min_length_error'      =>  'The :PARAM field must be at least 3 characters.',
+    'out_of_list_category'  =>  'The :PARAM field must be one of: business, entertainment, general, sports, technology, news, science.',
+    'missing_data_in_db'    =>  'The :PARAM field must exist in the news table.',
+    'date_format_error'     =>  'The :PARAM field must be in date format like Y-m-d.'
+];
